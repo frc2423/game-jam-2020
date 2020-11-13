77b936723cd57bd5ae40f68d97ee519b800ae531
alexthetuna <alexandrathuene@gmail.com>
2020-11-13 02:05:29 +0000

    moreBH

--- src/assets/js/black-hole.js
+++ src/assets/js/black-hole.js
@@ -2,17 +2,26 @@ import Sprite from './sprite';
 /*
 black hole should pull ship towards black hole.
 */
+let kForce = 1
 export default class BlackHole extends Sprite {
 
-  constructor(scene, x, y) {
+  constructor(scene, x, y, radius) {
     super(scene, x, y, 'ship');
+    this.radius = radius
   }
   getForce(ship) {
       let distance = ((this.getY() - ship.getY()) **2 + (this.getX() - ship.getX()) **2) **.5; 
-      let force = 
-if ()
+      let force =  this.radius *kForce / distance 
+      return force
   }
   attract(ship) {
-
+      let force = this.getForce (ship)
+      let o = ship.getY() - this.getY()
+      let a = ship.getX() - this.getX()
+      let angle = Math.atan2(o , a)
+      let x = force * Math.cos(angle) 
+      let y = force * Math.sin(angle) 
+      ship.x = ship.x + x 
+      ship.y = ship.y + y 
   }
 }
